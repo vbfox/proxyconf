4892060e8eabb9674c9c10dec60b248a69af364a
Julien Roncaglia <julien@roncaglia.fr>
2018-05-26 23:26:15 +0200

    Split cli in multiple files

---
+++ src/bin/proxyconf/args.rs
@@ -0,0 +1,54 @@
+use clap::{Arg, App, SubCommand};
+
+fn config_args<'a, 'b>(app: App<'a, 'b>) -> App<'a, 'b>{
+    app
+        .arg(
+            Arg::with_name("autodetect")
+            .short("a")
+            .long("auto-detect")
+            .help("Automatically detect settings")
+            .conflicts_with("setupscript")
+            .conflicts_with("server"))
+        .arg(
+            Arg::with_name("setupscript")
+            .short("S")
+            .long("setup-script")
+            .value_name("SCRIPT_URL")
+            .help("Setup script address")
+            .takes_value(true)
+            .conflicts_with("autodetect")
+            .conflicts_with("server"))
+        .arg(
+            Arg::with_name("server")
+            .short("s")
+            .long("server")
+            .value_name("ADDRESS:PORT")
+            .help("Use a manual proxy with the specified address and port")
+            .takes_value(true)
+            .conflicts_with("autodetect")
+            .conflicts_with("setupscript"))
+        .arg(
+            Arg::with_name("overrides")
+            .short("o")
+            .long("overrides")
+            .value_name("OVERRIDES")
+            .help("List of addresses that don't use the proxy (Separated by semicolons)")
+            .takes_value(true)
+            .conflicts_with("autodetect")
+            .conflicts_with("setupscript"))
+}
+
+pub fn get<'a, 'b>() -> App<'a, 'b> {
+    return
+        App::new("ProxyConf")
+        .version(crate_version!())
+        .author("Julien Roncaglia <julien@roncaglia.fr>")
+        .about("Windows proxy configuration from the command line")
+        .subcommand(
+            config_args(
+                SubCommand::with_name("set")
+                .about("Set the proxy configuration")))
+        .subcommand(
+            SubCommand::with_name("show")
+            .about("Show the current proxy configuration"));
+}
---
+++ src/bin/proxyconf/write_config.rs
@@ -0,0 +1,24 @@
+use proxyconf;
+use std::io;
+use std::io::Write;
+
+fn to_writer(writer: &mut Write, config: &proxyconf::ProxyConfig) {
+    if config.automatically_detect_settings {
+        writeln!(writer, "Automatically detect settings").unwrap();;
+    }
+    else if config.use_setup_script && config.setup_script_address.len() > 0 {
+        writeln!(writer, "Setup script address: {}", config.setup_script_address).unwrap();;
+    }
+    else if config.use_manual_proxy && config.manual_proxy_address.len() > 0{
+        writeln!(writer, "Manual proxy: {}", config.manual_proxy_address).unwrap();;
+        writeln!(writer, "Exceptions: {}", config.manual_proxy_overrides).unwrap();;
+    }
+    else {
+        writeln!(writer, "No proxy configured").unwrap();
+    }
+}
+
+pub fn to_stdout(config: &proxyconf::ProxyConfig) {
+    let mut stdout = io::stdout();
+    to_writer(&mut stdout, &config);
+}
