0d5cc96875fae1c549c9bc1d983c0e394b4a8a8d
Julien Roncaglia <julien@roncaglia.fr>
2018-07-08 21:58:50 +0200

    Make modern registry write configurable

--- src/proxyconf/ie/modern/registry.rs
+++ src/proxyconf/ie/modern/registry.rs
@@ -26,35 +26,50 @@ use winreg::{RegKey, RegValue};
 
 const KEY_PATH: &'static str =
     "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections";
-const VALUE_NAME: &'static str = "DefaultConnectionSettings";
+pub const DEFAULT_CONNECTION_NAME: &'static str = "DefaultConnectionSettings";
 
-fn open_key(write: bool) -> Result<RegKey> {
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+#[derive(Debug, Clone)]
+pub enum Target {
+    System,
+    CurrentUser
+}
+
+#[derive(Debug, Clone)]
+pub struct Location {
+    pub target: Target,
+    pub connection_name: String,
+}
+
+fn open_key(target: &Target, write: bool) -> Result<RegKey> {
+    let root_key = match target {
+        Target::System => RegKey::predef(HKEY_LOCAL_MACHINE),
+        Target::CurrentUser =>  RegKey::predef(HKEY_CURRENT_USER),
+    };
     let access = if write { KEY_ALL_ACCESS } else { KEY_READ };
-    let key = hkcu.open_subkey_with_flags(KEY_PATH, access)?;
+    let key = root_key.open_subkey_with_flags(KEY_PATH, access)?;
     return Ok(key);
 }
 
-fn write_raw(bytes: Vec<u8>) -> Result<()> {
+fn write_raw(location: &Location, bytes: Vec<u8>) -> Result<()> {
     let value = RegValue {
         vtype: REG_BINARY,
         bytes,
     };
-    let key = open_key(true)?;
-    key.set_raw_value(VALUE_NAME, &value)?;
+    let key = open_key(&location.target, true)?;
+    key.set_raw_value(&location.connection_name, &value)?;
     return Ok(());
 }
 
-pub fn write_full(config: &types::FullConfig) -> Result<()> {
+pub fn write_full(location: &Location, config: &types::FullConfig) -> Result<()> {
     let mut bytes = Vec::new();
     serialization::serialize(config, &mut bytes)?;
-    write_raw(bytes)?;
+    write_raw(location, bytes)?;
     return Ok(());
 }
 
-fn read_raw() -> Result<Vec<u8>> {
-    let key = open_key(false)?;
-    let value = key.get_raw_value(VALUE_NAME)?;
+fn read_raw(location: &Location) -> Result<Vec<u8>> {
+    let key = open_key(&location.target, false)?;
+    let value = key.get_raw_value(&location.connection_name)?;
 
     match value.vtype {
         REG_BINARY => Ok(value.bytes),
@@ -62,41 +77,41 @@ fn read_raw() -> Result<Vec<u8>> {
     }
 }
 
-pub fn read_full() -> Result<types::FullConfig> {
-    let bytes = read_raw()?;
+pub fn read_full(location: &Location) -> Result<types::FullConfig> {
+    let bytes = read_raw(location)?;
     let conf = serialization::deserialize(&bytes[..])?;
     return Ok(conf);
 }
 
-pub fn get_next_counter() -> u32 {
-    let full_result = read_full();
+pub fn get_next_counter(location: &Location) -> u32 {
+    let full_result = read_full(location);
     match full_result {
         Ok(full) => full.counter + 1,
         _ => 0
     }
 }
 
-pub fn read() -> Result<types::ProxyConfig> {
-    return Ok(read_full()?.config);
+pub fn read(location: &Location) -> Result<types::ProxyConfig> {
+    return Ok(read_full(location)?.config);
 }
 
-pub fn write(config: types::ProxyConfig) -> Result<()> {
-    let full_before = read_full()?;
+pub fn write(location: &Location, config: types::ProxyConfig) -> Result<()> {
+    let full_before = read_full(location)?;
     let full_after = types::FullConfig {
         version: super::IE7_VERSION,
         counter: full_before.counter + 1,
         config,
     };
-    write_full(&full_after)?;
+    write_full(location, &full_after)?;
 
     Ok(())
 }
 
-pub fn update<F>(updater: F) -> Result<()>
+pub fn update<F>(location: &Location, updater: F) -> Result<()>
 where
     F: FnOnce(types::ProxyConfig) -> types::ProxyConfig,
 {
-    let full_before = read_full()?;
+    let full_before = read_full(location)?;
     let after = updater(full_before.config);
 
     let full_after = types::FullConfig {
@@ -104,7 +119,7 @@ where
         counter: full_before.counter + 1,
         config: after,
     };
-    write_full(&full_after)?;
+    write_full(location, &full_after)?;
 
     Ok(())
 }
