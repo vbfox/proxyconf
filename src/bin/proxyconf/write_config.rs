aa6c7ff864487de7ae64d8eeb1d400cac83a092f
Julien Roncaglia <julien@roncaglia.fr>
2018-07-09 00:06:24 +0200

    Better "show"

--- src/bin/proxyconf/write_config.rs
+++ src/bin/proxyconf/write_config.rs
@@ -6,9 +6,9 @@ pub fn ie_modern(config: &internet_settings::modern::ProxyConfig) {
     } else if config.use_setup_script && config.setup_script_address.len() > 0 {
         println!("    Setup script address: {}", config.setup_script_address);
     } else if config.use_manual_proxy && config.manual_proxy_address.len() > 0 {
-        println!("    Manual proxy: {}", config.manual_proxy_address);
-        println!("    Bypass list: {}", config.manual_proxy_bypass_list);
+        println!("    Manual proxy : {}", config.manual_proxy_address);
+        println!("    Bypass list  : {}", config.manual_proxy_bypass_list);
     } else {
-        println!("    No proxy configured");
+        println!("    Direct access (no proxy server).");
     }
 }
